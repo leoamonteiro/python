9048872d67d5ef6d79a9f1fb19d06baf9c799e35
Leonardo monteiro <leonardoandremonteiro@gmail.com>
2023-04-18 20:43:27 -0300

    variaveis imput

---
+++ app.py
@@ -0,0 +1,7 @@
+nome = input ('qual e seu nome: ')
+idade = input ('qual e a sua idade: ')
+idade = str (idade)
+cidade = input('qual a cidade de nascimento: ')
+
+print(' o colaborador ' + nome +' tem ' + idade  + ' anos de idade e mora na cidade '+ cidade + ' . ')
+
